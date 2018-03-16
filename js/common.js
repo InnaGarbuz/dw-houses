97467ce8f935af4a5952d74ca0a042b0c2470f4d
igarbuz <inna.garbuz@accounts.ait.com>
2018-03-16 22:58:29 +0200

    dw

---
+++ js/common.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function($){
+
+  $('.menu-cont').on('click', function () {
+
+      $('.menu').toggleClass('active');
+      $('.dw-header__navbar').slideToggle(200);
+
+    });
+
+});
+
